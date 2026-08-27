356180e352202736f968426ac05375c651f55098
jerrymiegarcia18-code <jerrymiegarcia18@gmail.com>
2026-08-27 17:39:49 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# Program: Sum of Three Numbers
+# Get inputs
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+# Process
+total = num1 + num2 + num3
+
+# Output
+print(f"The sum of the three numbers is: {total}")
