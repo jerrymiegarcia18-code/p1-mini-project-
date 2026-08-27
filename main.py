f4ca0db5f18b78d72fb0dec7d57c08dec09c12d4
jerrymiegarcia18-code <jerrymiegarcia18@gmail.com>
2026-08-27 17:57:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # Program: Sum of Three Numbers
 # Get inputs
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-num3 = float(input("Enter the third number: "))
+num1 = float(input("5: "))
+num2 = float(input("10: "))
+num3 = float(input("15: "))
 
 # Process
 total = num1 + num2 + num3
 
 # Output
-print(f"The sum of the three numbers is: {total}")
+print(f"The sum of the three numbers is: {30}")
